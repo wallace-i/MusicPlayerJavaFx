87422363cc2cef1c8d91c68131b5d1eba18c57e2
hitmon-gg <97249957+hitmon-gg@users.noreply.github.com>
2023-03-21 12:02:22 -0700

    -Spruced up bug report module - set up email to send all bug reports to: bugreportsldev.gmail.com
    TODO:
    => css focus/hover listview tableview selection (like in default setting)
    => keybinds (space for play/pause etc)
    => change catch dialogue to system.out.println
    => listview responsiveness / pause-seeker thumb focus? requires focus then activates
    => test for bugs (check concurrency synchronized keywords)
    => write about/help
    => code cleanup
        - license
        - function headers
        - documentation
    => installable .exe
    --------------
    => Spotify API implementation?
    => Split tracklist into multiple files for sequential reading
    => web object to play youtube videos based on user spotify data

--- src/main/java/com/iandw/musicplayerjavafx/BugReportController.java
+++ src/main/java/com/iandw/musicplayerjavafx/BugReportController.java
@@ -1,7 +1,8 @@
 package com.iandw.musicplayerjavafx;
 
-import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,6 +10,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -18,6 +20,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.mail.*;
 
 public class BugReportController {
+    @FXML private AnchorPane anchorPane;
+    @FXML private TextField userNameField;
     @FXML private TextField userEmailField;
     @FXML private TextField subjectField;
     @FXML private TextArea textArea;
@@ -27,10 +31,9 @@ public class BugReportController {
     private ByteArrayOutputStream consoleOutput;
 
 
-
     public void initializeData(ByteArrayOutputStream consoleOutput) {
         this.consoleOutput = consoleOutput;
-
+        setTextFieldFocus();
     }
 
     public void showBugReportWindow(ByteArrayOutputStream consoleOutput) throws IOException {
@@ -45,17 +48,33 @@ public class BugReportController {
         stage.setTitle("Bug Report");
         stage.setResizable(false);
         stage.show();
+
+    }
+
+    private void setTextFieldFocus() {
+        final BooleanProperty firstTime = new SimpleBooleanProperty(true);
+
+        userNameField.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue && firstTime.get()) {
+                anchorPane.requestFocus();
+                firstTime.setValue(false);
+            }
+        });
     }
 
     @FXML
     private void insertConsoleLogClicked() {
-        textArea.setText(consoleOutput.toString());
+        // Add console log to bottom of text area
+        textArea.setText(textArea.getText() + '\n' + consoleOutput.toString());
     }
 
     @FXML
     private void sendClicked() {
-        final String devEmail = "iandw@spectrallines.dev";
+        statusLabel.setText("Sending message...");
+
         Dotenv dotenv = Dotenv.configure().load();
+        final String devEmail = dotenv.get("BUG_REPORT_EMAIL");
+        final String token = dotenv.get("TOKEN");
 
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
@@ -63,29 +82,40 @@ public class BugReportController {
         props.put("mail.smtp.starttls.enable", "true"); //TLS
         props.put("mail.smtp.auth", "true");
 
+        // Authenticates via dev bugreport gmail account
         Authenticator authenticator = new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(devEmail, dotenv.get("TOKEN"));
+                return new PasswordAuthentication(devEmail, token);
             }
         };
 
         Session session = Session.getInstance(props, authenticator);
 
-
         try {
             MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(userEmailField.getText()));
             message.setRecipients(Message.RecipientType.TO, devEmail);
             message.setSubject(subjectField.getText());
             message.setSentDate(new Date());
-            message.setText(textArea.getText());
+
+            // Add user email to top of bug report
+            message.setText(
+                    "From: " + userNameField.getText() + '\n' +
+                    "Email: " + userEmailField.getText() + '\n' +
+                    "App: MusicPlayer" + '\n' +
+                    textArea.getText()
+            );
+
+            // Send
             Transport.send(message);
 
             statusLabel.setText("Message sent!");
+            sendButton.disableProperty().set(true);
+            setTextFieldFocus();
 
         } catch (MessagingException mex) {
             statusLabel.setText("Send failed.");
             System.out.println("send failed, exception: " + mex);
+            sendButton.disableProperty().set(false);
         }
 
     }
